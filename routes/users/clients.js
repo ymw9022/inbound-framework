edecfb66abcc170e615e0704cb8864bd22658c97
Jinma li <lijinma@126.com>
2014-08-01 17:52:53 +0800

    - remove Larry's debug stuff

--- routes/users/clients.js
+++ routes/users/clients.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-console.log("111111111111111111111111111");
-
 /* GET clients listing. */
 router.get('/', function(req, res) {
   res.send('clients: respond with a resource');
