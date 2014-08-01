a17800c8c39716e8c91a8863852f5703696159d1
Jinma li <lijinma@126.com>
2014-08-01 17:52:39 +0800

    - temporarily disable 404

--- app.js
+++ app.js
@@ -39,11 +39,11 @@ dirWalker.walk(__dirname + "/routes", 0, function(path, floor) {
 });
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 
 /// error handlers
 
